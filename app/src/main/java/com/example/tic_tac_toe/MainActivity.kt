e64c9ed14a382adbe7ba762c34b558fdda67c5be
Ayush Paul <ayushpaul2002@gmail.com>
2021-12-06 16:46:04 +0530

    Added Initial Files

---
+++ app/src/main/java/com/example/tic_tac_toe/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.tic_tac_toe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() , View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
+    }
+}
